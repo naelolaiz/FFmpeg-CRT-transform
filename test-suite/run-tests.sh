7471adff59d29730e273453a95eb518148a9e210
nael <naelblog@gmail.com>
2023-02-04 15:53:11 +0100

    updated ci

--- test-suite/run-tests.sh
+++ test-suite/run-tests.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-#TESTRUNSTART=%time%
 TESTRUNSTART=$(date +%s.%N)
 
 echo rm *OLD* 2> /dev/null
 
 for filename in *out.*; do 
-    echo mv "${filename}" "${filename//\.*/}-OLD.${filename//?*\./.}"
+    mv "${filename}" "${filename//\.*/}-OLD.${filename//?*\./.}"
 done
 
 pushd ..
 for filename in test-suite/??.*; do 
     EXTENSION=${filename//?*\./.}
     BASENAME=${filename//\.*/}
-    echo ./ffcrt.sh ${filename//\.mp4/cfg.cfg} ${filename} ${BASENAME}-out${EXTENSION}
+    ./ffcrt.sh ${BASENAME}cfg.cfg ${filename} ${BASENAME}-out${EXTENSION}
 done
 popd
 ENDRUN=$(date +%s.%N)
@@ -20,4 +19,3 @@ ENDRUN=$(date +%s.%N)
 echo "TOTAL FOR ALL TESTS - "
 echo "Started:     ${TESTRUNSTART} Seconds"
 echo "Finished:    ${ENDRUN} Seconds"
-echo "Total:       $( echo "$end - $start" | bc -l ) Seconds"
